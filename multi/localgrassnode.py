324324842a1bf6fdc1a64f7a0e0ced96d11d1468
Pr4mpox <114173536+Brajamustii@users.noreply.github.com>
2024-11-01 23:01:57 +0700

    Update localgrassnode.py

--- multi/localgrassnode.py
+++ multi/localgrassnode.py
@@ -10,9 +10,8 @@ from loguru import logger
 from websockets_proxy import Proxy, proxy_connect
 from fake_useragent import UserAgent
 
-user_agent = UserAgent()
+user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
 random_user_agent = user_agent.random
-
 
 async def connect_to_wss(socks5_proxy, user_id):
     device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
@@ -27,7 +26,8 @@ async def connect_to_wss(socks5_proxy, user_id):
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
-            uri = "wss://proxy.wynd.network:4650/"
+            urilist = ["wss://proxy.wynd.network:4444/","wss://proxy.wynd.network:4650/"]
+            uri = random.choice(urilist)
             server_hostname = "proxy.wynd.network"
             proxy = Proxy.from_url(socks5_proxy)
             async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
@@ -38,7 +38,7 @@ async def connect_to_wss(socks5_proxy, user_id):
                             {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
                         logger.debug(send_message)
                         await websocket.send(send_message)
-                        await asyncio.sleep(20)
+                        await asyncio.sleep(5)
 
                 await asyncio.sleep(1)
                 asyncio.create_task(send_ping())
@@ -74,25 +74,21 @@ async def connect_to_wss(socks5_proxy, user_id):
 
 
 async def main():
-    # Membaca user_id dari file user_id.txt
     with open('user_id.txt', 'r') as user_file:
         user_ids = user_file.read().splitlines()
     
-    # Menampilkan jumlah akun
     print(f"Jumlah akun: {len(user_ids)}")
     
     with open('local_proxies.txt', 'r') as proxy_file:
         local_proxies = proxy_file.read().splitlines()
     
-    # Membuat tugas untuk setiap user_id dengan proxy secara bergantian
     tasks = []
     proxy_count = len(local_proxies)
     for index, user_id in enumerate(user_ids):
-        proxy = local_proxies[index % proxy_count]  # Memilih proxy secara bergantian
+        proxy = local_proxies[index % proxy_count]
         tasks.append(asyncio.ensure_future(connect_to_wss(proxy, user_id)))
     
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    #letsgo
     asyncio.run(main())
